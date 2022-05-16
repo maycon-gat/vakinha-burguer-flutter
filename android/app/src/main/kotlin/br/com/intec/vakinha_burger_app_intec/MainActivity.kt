eabbe4d87add18991b35e5fdb46c308dc09101dc
Maycon Henrique Gat <maycon.henrique.gat@gmail.com>
2022-05-16 10:32:49 -0300

    .

---
+++ android/app/src/main/kotlin/br/com/intec/vakinha_burger_app_intec/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.intec.vakinha_burger_app_intec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/auth/login/login_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger_app_intec/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_app_intec/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:vakinha_burger_app_intec/app/core/ui/widgets/vakinha_button.dart';
+import 'package:vakinha_burger_app_intec/app/core/ui/widgets/vakinha_textformfield.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: VakinhaAppbar(
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: LayoutBuilder(
+        builder: (_, constraints) {
+          return ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+                child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: context.textTheme.headline6?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: context.theme.primaryColorDark,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const VakinhaTextformfield(label: 'E-mail'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const VakinhaTextformfield(label: 'senha'),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: VakinhaButton(
+                        width: context.width,
+                        label: 'ENTRAR',
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Não possui uma conta?'),
+                        TextButton(
+                          onPressed: () {
+                            Get.toNamed('/auth/register');
+                          },
+                          child: const Text(
+                            'Cadastre-se',
+                            style: VakinhaUI.textBold,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )),
+          );
+        },
+      ),
+    );
+  }
+}
